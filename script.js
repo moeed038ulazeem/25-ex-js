2bc682681d258f27ddab8b4ca0a83d3a1804a5e0
moeed038ulazeem <moeedmoeed038@gamil.com>
2024-09-14 05:02:47 -0700

    js updated

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
-var changer = document.getElementById("hitMe");
-changer.addEventListener("click",function(){
-    var body1 = document.getElementById('b1');
-    if(body1.style.backgroundColor==="black"){
-        body1.style.backgroundColor = "white";
-        changer.innerText = "black mode";
-        changer.style.backgroundColor="black";
-        changer.style.color="white"
-    }
-    else{
-        body1.style.backgroundColor = "white";
-        changer.innerText = "white Mode";
-        changer.style.backgroundColor="black";
-        changer.style.color="white"
-    }
+// var changer = document.getElementById("hitMe");
+// changer.addEventListener("click",function(){
+//     var body1 = document.getElementById('b1');
+//     if(body1.style.backgroundColor==="black"){
+//         body1.style.backgroundColor = "white";
+//         changer.innerText = "black mode";
+//         changer.style.backgroundColor="black";
+//         changer.style.color="white"
+//     }
+//     else{
+//         body1.style.backgroundColor = "white";
+//         changer.innerText = "white Mode";
+//         changer.style.backgroundColor="black";
+//         changer.style.color="white"
+//     }
   
-})
+// })
+var changer = document.getElementById("hitMe");
+changer.addEventListener("click", function() {
+    var body1 = document.getElementById('b1');
+    
+    if (body1.style.backgroundColor === "black") {
+        // If the background color is black, change it to white
+        body1.style.backgroundColor = "white";
+        changer.innerText = "Dark Mode";
+        changer.style.backgroundColor = "black";
+        changer.style.color = "white";
+    } else {
+        // If the background color is not black (assumed to be white), change it to black
+        body1.style.backgroundColor = "black";
+        changer.innerText = "Light Mode";
+        changer.style.backgroundColor = "white";
+        changer.style.color = "black";
+    }
+});
